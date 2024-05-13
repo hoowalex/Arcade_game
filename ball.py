67ddeb8c20bfdb9ab8c8a506126e4faa3703cf14
Hoow <ipz222_roi@student.ztu.edu.ua>
2024-05-13 22:20:46 +0300

    the logic of the game is distributed by class

---
+++ ball.py
@@ -0,0 +1,34 @@
+import random
+
+
+class Ball():
+
+    def __init__(self, canvas, platform, color):
+        self.canvas = canvas
+        self.platform = platform
+        self.oval = canvas.create_oval(200, 200, 215, 215, fill=color)
+        self.dir = [-3, -2, -1, 1, 2, 3]
+        self.x = random.choice(self.dir)
+        self.y = -1
+        self.touch_bottom = False
+
+    def touch_platform(self, ball_pos):
+        platform_pos = self.canvas.coords(self.platform.rect)
+        if ball_pos[2] >= platform_pos[0] and ball_pos[0] <= platform_pos[2]:
+            if ball_pos[3] >= platform_pos[1] and ball_pos[3] <= platform_pos[3]:
+                return True
+        return False
+
+    def draw(self):
+        self.canvas.move(self.oval, self.x, self.y)
+        pos = self.canvas.coords(self.oval)
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= 400:
+            self.touch_bottom = True
+        if self.touch_platform(pos) == True:
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= 500:
+            self.x = -3
---
+++ platform.py
@@ -0,0 +1,23 @@
+class Platform():
+
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.rect = canvas.create_rectangle(230, 300, 330, 310, fill=color)
+        self.x = 0
+        self.canvas.bind_all('<KeyPress-Left>', self.left)
+        self.canvas.bind_all('<KeyPress-Right>', self.right)
+
+    def left(self, event):
+        self.x = -2
+
+    def right(self, event):
+        self.x = 2
+
+    def draw(self):
+        self.canvas.move(self.rect, self.x, 0)
+        pos=self.canvas.coords(self.rect)
+        if pos[0] <= 0:
+            self.x = 0
+        if pos[2] >= 500:
+            self.x = 0
+ # type: ignore
